46b1cc62418c575196f90eb42de4d74b9ff7c348
Kenneth Imade <kenneth.imade@yahoo.com>
2022-07-15 18:05:40 +0100

    setup db connection

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
-
+const dotenv = require("dotenv").config();
+const mysql = require('mysql2');
 const port = process.env.PORT || 5000;
 const app = express();
 
+// DB connection
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+console.log("Connected to PLANETSCALE")
+
 
 // Middlewares
-
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 // Routes
 
