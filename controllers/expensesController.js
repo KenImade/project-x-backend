e393fd1cc3000e77db0d527fc06af36350ceda29
Kenneth Imade <kenneth.imade@yahoo.com>
2022-07-23 15:18:39 +0100

    added swagger documentation

--- controllers/expensesController.js
+++ controllers/expensesController.js
@@ -26,18 +26,22 @@ const getAllExpenses = asyncHandler( async (req, res) => {
 // @params  description, amount, customerId
 const createExpense = asyncHandler( async (req, res) => {
     const {id} = req.user[0][0];
-    const {description, amount, customerId} = req.body;
-
-
-    if (!amount || !customerId || !id) {
+    if (!id) {
         res.status(400)
-        res.json({msg: "Invalid request"})
-        return;
+        throw new Error("User cannot be found")
+    }
+
+    const {description, amount, transactionDate, customerId} = req.body;
+
+
+    if (!amount || !customerId || !transactionDate || !id) {
+        res.status(400)
+        throw new Error("Invalid request")
     } 
     
     await db.promise().query(
-        `INSERT INTO expenses (user_id, description, amount, customer_id)
-        VALUES('${id}','${description}','${amount}','${customerId}')
+        `INSERT INTO expenses (user_id, description, amount, transaction_date, customer_id)
+        VALUES('${id}','${description}','${amount}','${transactionDate}','${customerId}')
     `)
     
     res.status(200).json({msg: "Expense has been added"})
@@ -72,45 +76,17 @@ const getExpense = asyncHandler(async (req, res) => {
     res.status(200).json({expense: expense[0][0]})
 })
 
-// @desc    Delete an expense
-// @route   DELETE /api/expenses/:id
-// @access  Private
-const deleteExpense = asyncHandler( async (req, res) => {
-    const expense = await db.promise().query(`
-        SELECT * FROM expenses where id = '${req.params.id}'
-    `)
-
-    if (expense[0].length === 0) {
-        res.status(400)
-        throw new Error("Expense not found")
-    }
-
-    // Check for user
-    if(!req.user[0][0]) {
-        res.status(401)
-        throw new Error("User not found")
-    }
-
-    // Make sure the logged in user matches the user_id in customer
-    if (expense[0][0].user_id !== req.user[0][0].id) {
-        res.status(401)
-        throw new Error("User not authorized")
-    }
-
-    await db.promise().query(`
-        DELETE FROM expenses WHERE id=${req.params.id} AND
-        user_id = ${req.user[0][0].id}
-    `)
-
-    res.status(200).json({id: req.params.id})
-})
-
 // @desc    Update an expense
 // @route   UPDATE /api/expenses/:id
 // @access  Private
 // @params  description, amount
 const updateExpense = asyncHandler( async (req, res) => {
-    const {description, amount} = req.body;
+    const {description, amount, transactionDate, customerId} = req.body;
+
+    if (!description || !amount || !transactionDate || !customerId) {
+        res.status(400)
+        throw new Error("Invalid parameters")
+    }
 
     const expense = await db.promise().query(`
         SELECT * FROM expenses where id = '${req.params.id}'
@@ -145,11 +121,46 @@ const updateExpense = asyncHandler( async (req, res) => {
     })
 })
 
+// @desc    Delete an expense
+// @route   DELETE /api/expenses/:id
+// @access  Private
+const deleteExpense = asyncHandler( async (req, res) => {
+    const expense = await db.promise().query(`
+        SELECT * FROM expenses where id = '${req.params.id}'
+    `)
+
+    if (expense[0].length === 0) {
+        res.status(400)
+        throw new Error("Expense not found")
+    }
+
+    // Check for user
+    if(!req.user[0][0]) {
+        res.status(401)
+        throw new Error("User not found")
+    }
+
+    // Make sure the logged in user matches the user_id in customer
+    if (expense[0][0].user_id !== req.user[0][0].id) {
+        res.status(401)
+        throw new Error("User not authorized")
+    }
+
+    await db.promise().query(`
+        DELETE FROM expenses WHERE id=${req.params.id} AND
+        user_id = ${req.user[0][0].id}
+    `)
+
+    res.status(200).json({
+        id: req.params.id,
+        msg: "Expense has been deleted"
+    })
+})
 
 module.exports = {
     getAllExpenses,
     createExpense,
     getExpense,
-    deleteExpense,
     updateExpense,
+    deleteExpense
 }
--- routes/expensesRoutes.js
+++ routes/expensesRoutes.js
@@ -11,10 +11,295 @@ const {
 } = require(path.join(__dirname,'..','controllers','expensesController.js'))
 const {protect} = require(path.join(__dirname,'..','middleware','authMiddleware.js'))
 
-router.post('/', protect, createExpense)
+/**
+ * @swagger
+ * components:
+ *  securitySchemes:
+ *      bearerAuth:
+ *          description: "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')"
+ *          type: apiKey
+ *          name: Authorization
+ *          in: header
+ *          bearerFormat: JWT
+ *  schemas:
+ *      Expenses:
+ *          type: object
+ *          required:
+ *              - id
+ *              - description
+ *              - amount
+ *              - transactionDate
+ *              - customerId
+ *          properties:
+ *              id:
+ *                  type: integer
+ *                  description: id of the expense
+ *              description:
+ *                  type: string
+ *                  description: description of the sale
+ *              amount:
+ *                  type: double
+ *                  description: expense amount
+ *              transactionDate:
+ *                  type: string
+ *                  description: date of expense
+ *              customerId:
+ *                  type: integer
+ *                  description: id of the customer
+ *          example:
+ *              description: "Expense made"
+ *              amount: 5000
+ *              transactionDate: "05-05-2020"
+ *              customerId: 1
+ *      error401:
+ *          type: string
+ *          example: '<html><body>Your HTML text</body></html>'
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Expenses
+ *  description: The expenses managing API
+ */
+
+/**
+ * @swagger
+ * /api/expenses:
+ *  get:
+ *      security:
+ *          - bearerAuth: []
+ *      summary: Gets all exenses for the user
+ *      tags: [Expenses]
+ *      responses:
+ *          200:
+ *              description: A JSON array of expense objects
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Expenses'
+ *          401:
+ *              description: Unauthorized
+ *              content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ *          404:
+ *              description: Expenses list was not found
+ *              content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ *          500:
+ *              description: Internal server error
+ *              content: 
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ */
 router.get('/', protect, getAllExpenses)
+
+/**
+ * @swagger
+ * /api/expenses:
+ *  post:
+ *      security:
+ *          - bearerAuth: []
+ *      summary: Creates a new expense
+ *      tags: [Expenses]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      Expense:
+ *                      type: object
+ *                      properties:
+ *                          description:
+ *                              type: string
+ *                              description: details of expense
+ *                          amount:
+ *                              type: double
+ *                              description: expense amount
+ *                          transactionDate:
+ *                              type: string
+ *                              description: date of expense
+ *                          customerId:
+ *                              type: integer
+ *                              description: id of customer
+ *      responses:
+ *          200:
+ *              description: The expense was added
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                              Message:
+ *                              type: object
+ *                              properties:
+ *                                  msg:
+ *                                      type: string
+ *                                      description: expense was added message
+ *          400:
+ *              description: Invalid request
+ *              content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ *          500:
+ *             description: Internal server error
+ *             content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401' 
+ */
+router.post('/', protect, createExpense)
+
+/**
+ * @swagger
+ * /api/expenses/{id}:
+ *  get:
+ *      security:
+ *          - bearerAuth: []
+ *      summary: Gets an expense by id
+ *      tags: [Expenses]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: integer
+ *              required: true
+ *              description: The expense id
+ *      responses:
+ *          200:
+ *              description: The expense description by id
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Expense'
+ *          404:
+ *              description: The expense was not found or does not belong to this customer
+ *              content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ *          401:
+ *              description: Unauthorized
+ *              content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ */
 router.get('/:id', protect, getExpense)
+
+/**
+ * @swagger
+ * /api/expenses/{id}:
+ *  put:
+ *      security:
+ *          - bearerAuth: []
+ *      summary: Update the expense by the id
+ *      tags: [Expenses]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: integer
+ *            required: true
+ *            description: The expense id
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      Expense:
+ *                      type: object
+ *                      properties:
+ *                          description:
+ *                                  type: string
+ *                                  description: expense details
+ *                          amount:
+ *                                  type: double
+ *                                  description: expense amount
+ *                          transactionDate:
+ *                                  type: string
+ *                                  description: date of expense
+ *                          customerId:
+ *                                  type: integer
+ *                                  description: id of customer
+ *      repsonses:
+ *          200:
+ *              description: The expense was updated
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          Message:
+ *                              type: object
+ *                              properties:
+ *                                  id:
+ *                                      type: integer
+ *                                      description: id of the updated expense
+ *                                  message:
+ *                                      type: string
+ *                                      description: update successful message
+ *          404:
+ *              description: The expense was not found
+ *              content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ *          401:
+ *              description: Unauthorized user
+ *              content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ *          500:
+ *              description: Interanl server error
+ *              content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ */
 router.put('/:id', protect, updateExpense)
+
+/**
+ * @swagger
+ * /api/expenses/{id}:
+ *  delete:
+ *      security:
+ *          bearerAuth: []
+ *      summary: Delete the expense by the id
+ *      tags: [Expenses]
+ *      parameters:
+ *          -   in: path
+ *              name: id
+ *              schema:
+ *                  type: integer
+ *              required: true
+ *              description: The expense id
+ *      responses:
+ *          200:
+ *              description: The customer was deleted
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          Message:
+ *                              type: object
+ *                              properties:
+ *                                  id:
+ *                                      type: integer
+ *                                      description: id of deleted expense
+ *                                  msg:
+ *                                      type: string
+ *                                      description: Delete confirmation message
+ *          404:
+ *              description: The expense was not found
+ *              content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ *          401:
+ *              description: Unauthorized
+ *              content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ *          500:
+ *              description: Internal serve error
+ *              content:
+ *                  text/html:
+ *                      $ref: '#/components/schemas/error401'
+ */
 router.delete('/:id', protect, deleteExpense)
 
 
